1121e597e2287250b1b2333552977a010c65bd93
diolan12 <dio_lantief21@outlook.com>
2022-04-04 00:19:21 +0700

    Update livedata.js
    - added debounce observer
    - added throttle observer

--- src/livedata.js
+++ src/livedata.js
@@ -26,9 +26,46 @@ class LiveData {
     }
     async observe(observer) {
         this.observers.add(observer);
-        if (this.value !== undefined && this.value !== null) {
-            observer(this.value);
-        }
+    }
+    async debounceObserve(observer, delay = 800) {
+        this.observers.add((() => {
+            let timeout
+
+            return (...args) => {
+                clearTimeout(timeout)
+                timeout = setTimeout(() => {
+                    observer(...args)
+                }, delay)
+            }
+        })());
+    }
+    async throttleObserve(observer, delay = 300) {
+        // this.observers.add(this.throttle(observer, delay));
+        this.observers.add((() => {
+            let shouldWait = false
+            let waitingArgs
+            const timeoutFunction = () => {
+                if (waitingArgs == undefined) {
+                    shouldWait = false
+                } else {
+                    observer(...waitingArgs)
+                    waitingArgs = undefined
+                    setTimeout(timeoutFunction, delay)
+                }
+            }
+
+            return (...args) => {
+                if (shouldWait) {
+                    waitingArgs = args
+                    return
+                }
+
+                observer(...args)
+                shouldWait = true
+
+                setTimeout(timeoutFunction, delay)
+            }
+        })());
     }
 
     notify() {
@@ -38,6 +75,41 @@ class LiveData {
                     observer(this.value);
                 }
             }
+        }
+    }
+    debounce(cb, delay) {
+        let timeout
+
+        return (...args) => {
+            clearTimeout(timeout)
+            timeout = setTimeout(() => {
+                cb(...args)
+            }, delay)
+        }
+    }
+    throttle(cb, delay) {
+        let shouldWait = false
+        let waitingArgs
+        const timeoutFunction = () => {
+            if (waitingArgs == undefined) {
+                shouldWait = false
+            } else {
+                cb(...waitingArgs)
+                waitingArgs = undefined
+                setTimeout(timeoutFunction, delay)
+            }
+        }
+
+        return (...args) => {
+            if (shouldWait) {
+                waitingArgs = args
+                return
+            }
+
+            cb(...args)
+            shouldWait = true
+
+            setTimeout(timeoutFunction, delay)
         }
     }
 }
